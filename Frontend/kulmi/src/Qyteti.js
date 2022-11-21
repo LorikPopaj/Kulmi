5e64eb53199a412f00ca8cbf55f4ace688d29ee6
LorikPopaj <lp51861@ubt-uni.net>
2022-11-21 18:44:26 +0100

    LorikReact

---
+++ Frontend/kulmi/src/Qyteti.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+export class Qyteti extends Component{
+    render(){
+        return(
+            <div>
+                <h3>This is Qyteti</h3>
+            </div>
+        )
+    }
+} 
